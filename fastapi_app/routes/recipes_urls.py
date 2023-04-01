c67718738ecdf12276ba6043e12e9824b062d6a1
mitrandir-pl <leshkopavel@gmail.com>
2023-04-01 15:11:29 +0300

    small fix in recipe urls

--- fastapi_app/routes/recipes_urls.py
+++ fastapi_app/routes/recipes_urls.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
 from config.db_settings import get_session
-from recipe_book import Recipe
 from DB import Neo4jSession
 
 router = APIRouter()
@@ -15,7 +14,6 @@ class RecipeName(BaseModel):
 @router.get("/recipes_name")
 async def get_recipes_by_name(recipe: RecipeName,
                               session: Neo4jSession = Depends(get_session)):
-    recipes = session.get_recipe_by_name(
-        recipe.name
-    )
+    """Returns a recipe by name"""
+    recipes = session.get_recipe_by_name(recipe.name)
     return recipes
